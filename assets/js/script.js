f141176858438119aa661f96835f98a16dc23d1d
AytajVV <aytajvv@code.edu.az>
2023-03-15 23:51:50 +0400

    feat:updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 let upload = document.getElementById("upload");
 let inputClick = document.getElementById("inputClick");
+let table = document.querySelector(".table");
 upload.addEventListener("click", function(){
     inputClick.click();
 });
@@ -10,11 +11,34 @@ inputClick.onchange=function(e){
         let reader = new FileReader();
         reader.onloadend=function(e){
             //console.log(e.target.result);
+            
             let tr = document.createElement("tr");
+
+            //Name
             let tdName = document.createElement("td");
             tdName.innerText = element.name;
+
+            //Image
             let tdImage = document.createElement("td");
-            tdImage.innerText = element.
+            let img = document.createElement("img");
+            img.setAttribute("src", e.target.result);
+            img.classList.add("imgStyle");
+            tdImage.append(img);
+
+            //Size
+            let tdSize = document.createElement("td");
+            tdSize.innerText = element.size;
+
+            //Settings
+            let tdSettings = document.createElement("td");
+            let icon = document.createElement("i");
+            //<i class="fa-solid fa-xmark"></i>
+            icon.classList.add("fa-solid", "fa-mark");
+
+            tr.append(tdName, tdImage, tdSize, tdSettings);
+
+            table.lastElementChild.append(tr);
+
         }
         reader.readAsDataURL(element);
     });
